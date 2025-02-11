efb4e822ad45483df70d07d96ff8ceb6ce022d7d
Daniel Perrefort <djperrefort@gmail.com>
2025-02-11 17:08:55 -0500

    Adds starter menubar layout page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { Routes } from "@angular/router";
 
 // Layout Components
 import { CenterLayoutComponent } from "~layouts/center-layout/center-layout.component";
-import { ErrorPageComponent } from "~pages/error-page/error-page.component";
+import { MenuBarLayoutComponent } from "~layouts/menu-bar-layout/menu-bar-layout.component";
 
 // Page Components
+import { ErrorPageComponent } from "~pages/error-page/error-page.component";
 import { HelloPageComponent } from "~pages/hello-page/hello-page.component";
 
 export const routes: Routes = [
   {
     path: "",
-    component: CenterLayoutComponent,
+    component: MenuBarLayoutComponent,
     children: [
       {path: "", component: HelloPageComponent}
     ],
---
+++ src/app/layouts/menu-bar-layout/menu-bar-layout.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { MatToolbar } from "@angular/material/toolbar";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { MetadataService } from "~services/metadata/metadata.service";
+
+/** Page layout with a navigation toolbar and page footer. */
+@Component({
+  selector: "app-menu-bar-layout",
+  templateUrl: "menu-bar-layout.component.html",
+  styleUrl: "menu-bar-layout.component.scss",
+  imports: [
+    MatToolbar,
+    RouterLink,
+    RouterOutlet,
+  ],
+})
+export class MenuBarLayoutComponent implements OnInit {
+  protected version!: string;
+
+  constructor(private metadataService: MetadataService) {}
+
+  /** Load application metadata, including the title and version number. */
+  ngOnInit(): void {
+    this.metadataService.getVersion().subscribe(
+      version => this.version = version
+    );
+  }
+}
