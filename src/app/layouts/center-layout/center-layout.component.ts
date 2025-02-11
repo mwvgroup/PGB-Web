f4488c42fe6e9a6da31555a005248b3eff4b153c
Daniel Perrefort <djperrefort@gmail.com>
2025-02-11 14:47:54 -0500

    Adds layout component for page-centered content

---
+++ src/app/layouts/center-layout/center-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+/** Minimal layout component for centering content in the middle of the page. */
+@Component({
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: "center-layout.component.html",
+  styleUrl: "center-layout.component.scss",
+})
+export class CenterLayoutComponent {}
