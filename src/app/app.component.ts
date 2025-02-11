53d2bacf069f8b7dc66bf672fe01737a3ad44757
Daniel Perrefort <djperrefort@gmail.com>
2025-02-11 16:15:02 -0500

    Consolidate application component into single file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: "<router-outlet />"
 })
-export class AppComponent {
-  title = 'PGB-Web';
-}
+export class AppComponent {}
