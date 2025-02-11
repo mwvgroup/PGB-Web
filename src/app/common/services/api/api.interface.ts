bcff9ed60454ca88319be10f55aa980d5b4ccdd8
Daniel Perrefort <djperrefort@gmail.com>
2025-02-11 14:30:47 -0500

    Removes unused interfaces

--- src/app/common/services/api/api.interface.ts
+++ src/app/common/services/api/api.interface.ts
@@ -5,26 +5,3 @@ export interface RequestOptions {
   headers?: HttpHeaders;
   params?: HttpParams;
 }
-
-/** Schema map for a single database column as returned by the API. */
-export interface SchemaColumn {
-  type: string;
-  nullable: boolean;
-  default: string;
-  primary_key: boolean;
-}
-
-/** Schema map for a single database table as returned by the API. */
-export interface SchemaTable {
-  columns: {
-    [key: string]: SchemaColumn
-  };
-}
-
-/** Schema map for the entire backend database as returned by the API. */
-export interface Schema {
-  tables: {
-    [key: string]: SchemaTable
-  };
-}
-
