f705ede290540d61d6b3de3c8b4e97cbf7f90293
Daniel Perrefort <djperrefort@gmail.com>
2025-02-11 14:40:51 -0500

    Adds hello world page component

---
+++ src/app/pages/hello-page/hello-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+/** A simple placeholder page. */
+@Component({
+  selector: "landing-page",
+  template: "Hello World!",
+})
+export class HelloPageComponent {}
