cb84d2990d6319565565d08a94155950f6079c70
Daniel Perrefort <djperrefort@gmail.com>
2025-02-12 11:47:53 -0500

    Adds feature cards to landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from "@angular/core";
 
 import { ContentSectionComponent } from "~components/content-section/content-section.component";
+import { FeatureCardComponent } from "~components/feature-card/feature-card.component";
 import { HeroSectionComponent } from "~components/hero-section/hero-section.component";
 
-/** A simple placeholder page. */
+/**
+ * Application landing page, providing a general project overview and links
+ * to additional project documentation/resources.
+ */
 @Component({
   templateUrl: "landing-page.component.html",
+  styleUrl: "landing-page.component.scss",
   imports: [
     ContentSectionComponent,
+    FeatureCardComponent,
     HeroSectionComponent,
   ],
 })
