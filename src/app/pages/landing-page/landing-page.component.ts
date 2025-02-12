304999c5ed6784c30a34c2e400ae0955b1fd222d
Daniel Perrefort <djperrefort@gmail.com>
2025-02-12 09:55:11 -0500

    Adds starter for hero section component

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 
 import { ContentSectionComponent } from "~components/content-section/content-section.component";
+import { HeroSectionComponent } from "~components/hero-section/hero-section.component";
 
 /** A simple placeholder page. */
 @Component({
   templateUrl: "landing-page.component.html",
   imports: [
-    ContentSectionComponent
+    ContentSectionComponent,
+    HeroSectionComponent
   ],
 })
 export class LandingPageComponent {}
