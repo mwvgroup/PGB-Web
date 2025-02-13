508acd8d4140f800600bd5addf428f3c7617b248
Daniel Perrefort <djperrefort@gmail.com>
2025-02-13 13:49:13 -0500

    Makes feature cards into links

--- src/app/common/components/feature-card/feature-card.component.ts
+++ src/app/common/components/feature-card/feature-card.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
 
 @Component({
-  selector: 'app-feature-card',
+  selector: "app-feature-card",
   imports: [
     MatCard,
     MatCardContent,
     MatCardHeader,
     MatCardTitle,
   ],
-  templateUrl: './feature-card.component.html',
-  styleUrl: './feature-card.component.scss'
+  templateUrl: "feature-card.component.html",
+  styleUrl: "feature-card.component.scss"
 })
 export class FeatureCardComponent {
-
+  @Input() href?: string;
 }
